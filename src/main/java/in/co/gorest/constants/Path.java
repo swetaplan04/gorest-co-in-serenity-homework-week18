39da6ee7e3c868cd0998d43e6639b5c153718f52
swetaplan04 <swetaplan04@gmail.com>
2022-06-07 22:21:57 +0100

    gorest-co-in-serenity-homework-week18

---
+++ src/main/java/in/co/gorest/constants/Path.java
@@ -0,0 +1,9 @@
+package in.co.gorest.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+
+    public static final String PATH = "";
+}
---
+++ src/test/java/in/co/gorest/testbase/TestBase.java
@@ -0,0 +1,19 @@
+package in.co.gorest.testbase;
+
+import in.co.gorest.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+/**
+ * Created by Jay
+ */
+public class TestBase {
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+    }
+
+}
